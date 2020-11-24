7031b1d204a79e8ae7af17a8d7d5c8ea13515bbf
rtosman <rennieallen@gmail.com>
2020-11-24 14:57:02 -0800

    Added support for building 32 bit or 64 bit targets

--- premake5.lua
+++ premake5.lua
@@ -5,10 +5,6 @@ workspace "ASIO"
       "E:/Users/Rennie Allen/source/include/asio-1.18.0/include",
       "%{prj.location}/common",
       "%{prj.location}/one",
-      "E:/Users/Rennie Allen/source/opensource/include/botan-2"
-   }
-   libdirs {
-      "E:/Users/Rennie Allen/source/opensource/lib"
    }
    links { "botan" }
 
@@ -18,6 +14,24 @@ workspace "ASIO"
       toolset('clang')
       defines { "_WIN32_WINNT=0x0601" }
       platforms { "Win32", "Win64" }
+
+   filter "platforms:Win32"
+      includedirs {
+         "E:/Users/Rennie Allen/source/opensource/include/botan-2/x86"
+      }
+      libdirs {
+         "E:/Users/Rennie Allen/source/opensource/lib/x86"
+      }
+      architecture "x32"
+  
+   filter "platforms:Win64"
+      includedirs {
+         "E:/Users/Rennie Allen/source/opensource/include/botan-2/x64"
+      }
+      libdirs {
+         "E:/Users/Rennie Allen/source/opensource/lib/x64",
+      }
+      architecture "x64"
 
 project "ServerOne"
    kind "ConsoleApp"
@@ -33,16 +47,18 @@ project "ServerOne"
       defines { "NDEBUG" }
       optimize "On"
 
-project "ClientOne"
-      kind "ConsoleApp"
-      targetdir "bin/%{cfg.buildcfg}"
 
-      files { "clientone/**.hpp", "clientone/**.cpp" }
-   
-      filter "configurations:Debug"
-         defines { "DEBUG" }
-         symbols "On"
-   
-      filter "configurations:Release"
-         defines { "NDEBUG" }
-         optimize "On"
+project "ClientOne"
+   kind "ConsoleApp"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "clientone/**.hpp", "clientone/**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+      
